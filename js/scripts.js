e0f2e11a5dcc8d8a2885ca0c45c5b638d8765b16
shane ryan <onyx.shane@gmail.com>
2017-11-03 13:57:50 -0700

    add backend flawed logic and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
 //Business
+
 function Pizza(size, topping) {
   this.size = size;
   this.topping = topping;
-  this.sizePrice = sizePrice;
-  this.toppingPrice = toppingPrice;
-  this.setPizzaPrice = setPizzaPrice;
-};
-
-Pizza.prototype.yourSize = function() {
-  
-  return this.sizePrice;
-};
-Pizza.prototype.yourTopping = function(){
-
-  return this.toppingPrice;
+  this.sizePrice;
+  this.toppingPrice;
+  this.setPizzaPrice();
+}
+Pizza.prototype.setPizzaPrice = function(){
+  return this.sizePrice + this.toppingPrice;
 }
 
-Pizza.prototype.setPizzaPrice = function(){
+Pizza.prototype.yourSize = function() {
+  if (this.size === "sm"){
+    this.sizePrice = 10.75;
+  }else if (this.size === "md"){
+    this.sizePrice = 14.25;
+  }else if (this.size === "lg"){
+    this.sizePrice = 20.25;
+  };
+}
 
- return this.size + this.topping
-};
+Pizza.prototype.yourTopping = function(){
+  if (this.topping === "top-1"){
+    this.toppingPrice = 4;
+  }else if(this.topping === "top-4"){
+    this.toppingPrice = 8;
+  }else if(this.topping === "top-5"){
+    this.toppingPrice = 11;
+  };
+
+}
+
 
 //prototype for displaying user input
 
@@ -28,10 +40,12 @@ $(document).ready(function() {
   $("#button").click(function(event) {
     event.preventDefault();
 
-    var selectedSize = $("select#pizza-size").val();
-    var selectedTopping = $("select#pizza-topping").val();
-    console.log(selectedTopping);
+    var checkedSize = $("select#pizza-size").val();
+    var checkedTopping = $("select#pizza-topping").val();
 
-    var pizza = new Pizza();
+
+    var pizza = new Pizza(checkedSize, checkedTopping);
+
+    $("ul#your-pizza").append("<li>" + pizza.setPizzaPrice() + "</li>");
   });
 });
