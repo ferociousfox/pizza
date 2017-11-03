2a657ccb0c5cb7d3f83b0d3beb722f11541c617b
shane ryan <onyx.shane@gmail.com>
2017-11-03 14:58:31 -0700

    more flawed logic ugh

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,31 @@ function Pizza(size, topping) {
   this.toppingPrice;
   this.setPizzaPrice();
 }
-Pizza.prototype.setPizzaPrice = function(){
-  return this.sizePrice + this.toppingPrice;
-}
+
 
 Pizza.prototype.yourSize = function() {
-  if (this.size === "sm"){
-    this.sizePrice = 10.75;
-  }else if (this.size === "md"){
-    this.sizePrice = 14.25;
-  }else if (this.size === "lg"){
-    this.sizePrice = 20.25;
+  if (this.size === 10){
+    this.sizePrice = 10;
+  }else if (this.size === 14){
+    this.sizePrice = 14;
+  }else if (this.size === 20){
+    this.sizePrice = 20;
   };
 }
 
 Pizza.prototype.yourTopping = function(){
-  if (this.topping === "top-1"){
+  if (this.topping === 1){
     this.toppingPrice = 4;
-  }else if(this.topping === "top-4"){
+  }else if(this.topping === 4){
     this.toppingPrice = 8;
-  }else if(this.topping === "top-5"){
+  }else if(this.topping === 5){
     this.toppingPrice = 11;
   };
-
 }
 
+Pizza.prototype.setPizzaPrice = function(){
+  return this.sizePrice + this.toppingPrice;
+}
 
 //prototype for displaying user input
 
@@ -40,11 +40,11 @@ $(document).ready(function() {
   $("#button").click(function(event) {
     event.preventDefault();
 
-    var checkedSize = $("select#pizza-size").val();
-    var checkedTopping = $("select#pizza-topping").val();
-
-
+    var checkedSize = parseInt($("select#pizza-size").val());
+    var checkedTopping = parseInt($("select#pizza-topping").val());
     var pizza = new Pizza(checkedSize, checkedTopping);
+    console.log(checkedSize);
+    console.log(checkedTopping);
 
     $("ul#your-pizza").append("<li>" + pizza.setPizzaPrice() + "</li>");
   });
